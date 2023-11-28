205e45dfd7ebf82ff80cc885b17d133268591944
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2023-11-28 13:04:17 +0200

    change web link

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "6976582329:AAEjk_us2V6q5HapRNwV4QaSNfurCr7c9jU";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "shoozia-telegram-web-shop.vercel.app";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
